bdc4d76556d209e2ba1cf64b290dd98fdf8b885e
Ferox250309 <134328153+Ferox250309@users.noreply.github.com>
2023-05-25 11:38:47 +0200

    Update Starwarsweather.py

--- Starwarsweather.py
+++ Starwarsweather.py
@@ -16,7 +16,7 @@ st.markdown("<div style='display: flex; flex-direction: column; justify-content:
 
 response = requests.get("https://api.weatherapi.com/v1/current.json?key=36fec4787ae4493db02202139232005&q=Aarhus&aqi=no")
 
-celsius = temperature + "°C"
+celsius = str(temperature) + "°C"
 
 st.subheader(celsius)
 
